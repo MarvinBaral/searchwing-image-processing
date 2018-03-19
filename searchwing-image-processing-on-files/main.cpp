41be1ceb44e8985878ee9cada89baab024c1745b
Marvin Baral <mba@jcbachmann.de>
2018-03-19 06:49:49 +0100

    Sorted contours by enclosed area. Added option to display the x-largest contours with the number keys. 0 = show only original image

--- searchwing-image-processing-on-files/main.cpp
+++ searchwing-image-processing-on-files/main.cpp
@@ -19,6 +19,7 @@ using namespace cv;
 const string WINDOW_NAME = "Contour Detection";
 const string DEBUG_WINDOW_NAME = "Debug";
 cv::Mat global_image;
+unsigned short num_showed_contours = 1;
 
 void getFiles(QString path, std::vector<QString> &images, std::vector<QString> &videos) { //!!! Hard string comparison with jpg, jpeg and mp4 !!!
 	QDirIterator iterator(path, QDirIterator::Subdirectories);
@@ -32,8 +33,26 @@ void getFiles(QString path, std::vector<QString> &images, std::vector<QString> &
 	}
 }
 
+//area comparison func
+bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
+	double i = fabs( contourArea(cv::Mat(contour1)) );
+	double j = fabs( contourArea(cv::Mat(contour2)) );
+	return ( i < j );
+}
+//bool compareContourSize ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
+//	int i = contour1.size();
+//	int j = contour2.size();
+//	return ( i < j );
+//}
+
 Mat contourDetection(Mat img) {
+	cv::Scalar green = cv::Scalar(0, 255, 0);
+	cv::Scalar red = cv::Scalar(0, 0, 255);
 	Mat working_hsv_image;
+	Mat displayed_image;
+	img.copyTo(displayed_image);
+
+	//thresholding
 	cv::cvtColor(img, working_hsv_image, CV_BGR2HSV);
 //	cv::medianBlur(working_hsv_image, working_hsv_image, 45);
 	std::vector<short int> lowerBounds = {90, 0, 0};
@@ -41,29 +60,27 @@ Mat contourDetection(Mat img) {
 	cv::inRange(working_hsv_image, lowerBounds, upperBounds, working_hsv_image);
 	cv::bitwise_not(working_hsv_image, working_hsv_image);
 
+	//find and draw contours
 	std::vector<std::vector<cv::Point>> contours;
 	cv::findContours(working_hsv_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-	cv::Scalar color = cv::Scalar(0, 255, 0);
-	for (int i = 0; i < contours.size(); i++) {
-		cv::drawContours(img, contours, i, color);
-	}
-	//display longest contour
-	cv::Scalar color2 = cv::Scalar(0, 0, 255);
-	int maxContourLength = 0;
-	int longestContour = 0;
-	int i = 0;
-	for (; i < contours.size(); i++) {
-		if ((int) contours[i].size() > maxContourLength) {
-			maxContourLength = std::max(maxContourLength, (int) contours[i].size());
-			longestContour = i;
+	if (num_showed_contours > 0) {
+		for (unsigned int i = 0; i < contours.size(); i++) {
+			cv::drawContours(displayed_image, contours, i, green);
 		}
 	}
-//	cv::drawContours(img, contours, longestContour, color2);
-	cv::rectangle(img, cv::boundingRect(contours[longestContour]), color2, 10);
+
+	// sort contours ascending by area https://stackoverflow.com/questions/13495207/opencv-c-sorting-contours-by-their-contourarea
+	std::sort(contours.begin(), contours.end(), compareContourAreas);
+
+	//display longest contours
+	for (unsigned short i = contours.size() - 1; i > (contours.size() - 1 - num_showed_contours); i--) {
+		//	cv::drawContours(img, contours, i, red);
+		cv::rectangle(displayed_image, cv::boundingRect(contours[i]), red, 10);
+	}
 
 	cout << "number of contours " << contours.size() << endl;
 	imshow(DEBUG_WINDOW_NAME, working_hsv_image);
-	return img;
+	return displayed_image;
 }
 
 void update() {
@@ -118,6 +135,16 @@ int main(int argc, char *argv[])
 				case 117: //u
 					update();
 					break;
+				case 48: num_showed_contours = 0; update(); break; //0, to clear all drawin info and only show original image
+				case 49: num_showed_contours = 1; update(); break; //1
+				case 50: num_showed_contours = 2; update(); break; //2
+				case 51: num_showed_contours = 3; update(); break; //3
+				case 52: num_showed_contours = 4; update(); break; //4
+				case 53: num_showed_contours = 5; update(); break; //5
+				case 54: num_showed_contours = 6; update(); break; //6
+				case 55: num_showed_contours = 7; update(); break; //7
+				case 56: num_showed_contours = 8; update(); break; //8
+				case 57: num_showed_contours = 9; update(); break; //9
 				default:
 					cout << keyPressed << endl;
 			}
